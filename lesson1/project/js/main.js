fb84ef646c8e4518765e4f64e56ac3386f5d6024
Kamol <tskamol@yandex.ru>
2020-01-10 17:39:50 +0300

    доделки по первому уроку

--- lesson1/project/js/main.js
+++ lesson1/project/js/main.js
@@ -4,24 +4,25 @@ const products = [
   {id: 1, title: 'Notebook', price: 20000},
   {id: 2, title: 'Mouse', price: 1500},
   {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
+  {id: 4, title: 'Gamepad'},
 ];
 
-const renderProduct = (title, price) => {
-  return `<div class="product-item">
+const renderProduct = (item, img = 'https://place-hold.it/200', price = 'no price', title = 'no title') =>
+   `<div class="product-item">
+            <img src="${img}" alt="">
             <h3>${title}</h3>
             <p>${price}</p>
             <button class="by-btn">Добавить в корзину</button>
           </div>`;
-};
 
-const renderProducts = (list, title="Значение заголовка по умолчанию", price= "Значение прайс по умолчанию 0") => {
-  const productList = list.map(product => renderProduct(product.title, product.price)).join('');
+
+const renderProducts = list => {
+  document.querySelector('.products').insertAdjacentHTML("beforeend", list.map(item => renderProduct(item, item.img, item.price, item.title)).join(''));
   // const productList = list.map(function (product) {
   //   return renderProduct(product.title, product.price)
   // });
   // console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+  // document.querySelector('.products').innerHTML = productList;
 };
 
 renderProducts(products);
